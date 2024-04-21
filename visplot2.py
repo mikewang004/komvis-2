40936b1392fa18eab28cf060dc7f34b9987ee55c
viperfish-oc <71723888+viperfish-oc@users.noreply.github.com>
2024-04-21 05:11:48 +0200

    interaction now working

---
+++ visplot2.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_magnetisation(magnetisation):
+    # plt.scatter(temps, magnetisation[:, -1]/N**2)
+    plt.plot(magnetisation / N**2)
+    plt.title("Mean magnetisation")
+    plt.xlabel("time")
+    plt.ylabel("magnetisation")
+    # plt.legend()
+    plt.show()
+
+
+def plot_magnetisation_multiple_temps(magnetisation, temps):
+    plt.scatter(temps, np.abs(magnetisation[:, -1] / N**2))
+    plt.title("Mean magnetisation")
+    plt.xlabel("temperature")
+    plt.ylabel("magnetisation")
+    # plt.legend()
+    plt.show()
+
+
+def plot_lattice(lattice_start, lattice_end):
+    # Plot lattice
+    plt.matshow(lattice_start)
+    plt.show()
+
+    plt.matshow(lattice_end)
+    plt.show()
+
+
+def plot_lattice_parallel(lattice_start, lattice_end):
+    fig, axis = plt.subplots(1, 2, layout="constrained", sharex=True, sharey=True)
+    axis[0].set(title="old")
+    axis[0].matshow(lattice_start)
+    axis[1].set(title="new")
+    axis[1].matshow(lattice_end)
+    plt.show()

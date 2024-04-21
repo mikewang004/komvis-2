21b7431183d76c366fe3a6e3452726d533f2f1ae
viperfish-oc <71723888+viperfish-oc@users.noreply.github.com>
2024-04-21 21:08:23 +0200

    fix magnetisation over time

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from visplot2 import *
 J = 1
 kb = 1
 
+
 class Lattice:
     def __init__(self, n_spins, temperature, init_type="random"):
         """Lattice of spins with a number of lattice operations
@@ -19,18 +20,22 @@ class Lattice:
 
         self.n_spins = n_spins
         self.temperature = temperature
+        self.init_type = init_type
         self.rng = np.random.default_rng()
-        self.spingrid = self.initialize(init_type)
+        self.spingrid = self.initialize()
         self.proposed_state = np.zeros((self.n_spins, self.n_spins))
         self.energy = self.get_initial_energy(self.spingrid)
+        self.magnetisation = 0
         self.latest_attempted_flip = (0, 0)
         self.J = 1
         self.H = 1
 
         return
 
-    def initialize(self, init_type = "random"):
-        if init_type == "random":
+    def initialize(
+        self,
+    ):
+        if self.init_type == "random":
             generated_state = (
                 2 * self.rng.integers(low=0, high=2, size=(self.n_spins, self.n_spins))
                 - 1
@@ -70,7 +75,7 @@ class Lattice:
                 lattice_product[k, :] = lattice_shift * lattice
                 k = k + 1
         self.energy = -J * np.sum(lattice_product)
-        return 0;
+        return 0
 
     def revert_proposed_state(self):
         self.spingrid[self.latest_attempted_flip] = (
@@ -100,7 +105,6 @@ class Lattice:
         return delta_energy
 
     def validate_or_revert_proposition(self):
-        #k_b = 1.3806503e-23
         k_b = 1
         delta_energy = self.get_proposed_delta_energy()
         beta = 1 / (k_b * self.temperature)
@@ -113,16 +117,18 @@ class Lattice:
             )
             if random_accept:
                 self.energy = self.energy + delta_energy
-                return 0;
+                return 0
             else:
                 self.revert_proposed_state()
-                return 0;
+                return 0
 
-
-    def get_magnetisation(self, lattice):
-        """Calculates total magnetisation according to M = sum_i s_i"""
-        return np.sum(lattice)
-
+    def update_magnetisation(self):
+        lattice = self.spingrid
+        """Calcnp.sum(lattice)ulates total magnetisation according to M = sum_i s_i"""
+        magnetisation = np.sum(self.spingrid)
+        print(magnetisation)
+        self.magnetisation = magnetisation
+        return 0
 
 
 class Simulation:
@@ -131,42 +137,50 @@ class Simulation:
         self.n_timesteps = n_timesteps
         self.results = Results()
 
+    def initialize_simulation(self):
+        self.results.magnetisation_over_time = np.zeros(self.n_timesteps)
+        return
 
     def run_simulation(self):
-        self.results.magnetisation = np.zeros(self.n_timesteps)
+        self.initialize_simulation()
+
         for t in tqdm(range(0, self.n_timesteps), desc="runnin"):
             self.system.generate_proposed_state()
             self.system.validate_or_revert_proposition()
-            self.results.magnetisation[t] = self.system.get_magnetisation(self.system.spingrid)
-        return 0;
+            self.system.update_magnetisation()
+            print(f"{np.copy(self.system.magnetisation)= }")
+            self.results.magnetisation_over_time[t] = self.system.magnetisation
+        return 0
 
-
-
-    def run_multiple_temperatures(self, temps):
-        """temps input as array"""
+    def run_multiple_temperatures(self, n_reps=1):
+        temps = np.linspace(1.0, 4.0, 10)
         magnetisation_multiple_temps = np.zeros([len(temps), self.n_timesteps])
         i = 0
         for temp in temps:
             self.system.temperature = temp
-            self.run_simulation()
-            magnetisation_multiple_temps[i, :] = self.results.magnetisation
             self.system.initialize()
+            self.run_simulation()
+            print(f"{self.results.magnetisation_over_time=}")
+            magnetisation_multiple_temps[i, :] = self.results.magnetisation_over_time
             i = i + 1
         self.results.magnetisation_multiple_temps = magnetisation_multiple_temps
 
-    def run_multiple_times_multiple_temperatures(self, temps, n_reps = 5):
-        magetisation_multiple_reps = np.zeros([n_reps, len(temps), self.n_timesteps])
-        for n in range(0, n_reps):
-            self.run_multiple_temperatures(temps)
-            magetisation_multiple_reps[n, :, :] = self.results.magnetisation_multiple_temps
-        
-        # Now calculate error over time 
-        mag_avg_over_reps = np.mean(magetisation_multiple_reps, axis = 0)
-        mag_std_over_reps = np.std(np.abs(magetisation_multiple_reps), axis = 0)
-        np.savetxt("mag_avg_test.txt", mag_avg_over_reps)
-        np.savetxt("mag_std_test.txt", mag_std_over_reps)
-        self.results.mag_avg_over_reps = mag_avg_over_reps
-        self.results.mag_std_over_reps = mag_std_over_reps
+    # def run_multiple_times_multiple_temperatures(self, temps, n_reps=5):
+    #     magetisation_multiple_reps = np.zeros([n_reps, len(temps), self.n_timesteps])
+    #     for n in range(0, n_reps):
+    #         self.run_multiple_temperatures(temps)
+    #         magetisation_multiple_reps[n, :, :] = (
+    #             self.results.magnetisation_multiple_temps
+    #         )
+    #
+    #     # Now calculate error over time
+    #     mag_avg_over_reps = np.mean(magetisation_multiple_reps, axis=0)
+    #     mag_std_over_reps = np.std(np.abs(magetisation_multiple_reps), axis=0)
+    #
+    # np.savetxt("mag_avg_test.txt", mag_avg_over_reps)
+    # np.savetxt("mag_std_test.txt", mag_std_over_reps)
+    # self.results.mag_avg_over_reps = mag_avg_over_reps
+    # self.results.mag_std_over_reps = mag_std_over_reps
 
 
 class Results(object):
@@ -174,20 +188,22 @@ class Results(object):
         return
 
 
-
 def main():
-    n_spins = 50
-    temperature = 1.5; temps = np.linspace(1.0, 4.0, 10)
-    n_timesteps = 1000000
+    n_spins = 5
+    temperature = 1.5
+    n_timesteps = 5
     lattice = Lattice(n_spins, temperature)
-    lattice_start = np.copy(lattice.spingrid)
     simulation = Simulation(lattice, n_timesteps)
-    #simulation.run_simulation()
-    #simulation.run_multiple_temperatures(temps)
-    simulation.run_multiple_times_multiple_temperatures(temps)
-    lattice_end = simulation.system.spingrid
-    plot_magnetisation_multiple_temps(simulation.results.mag_avg_over_reps, temps, n_spins, std = simulation.results.mag_std_over_reps)
-    #plot_lattice_parallel(lattice_start, lattice_end)
+    simulation.run_multiple_temperatures()
+    print(simulation.results.magnetisation_multiple_temps)
+
+    # plot_magnetisation_multiple_temps(
+    #     simulation.results.mag_avg_over_reps,
+    #     temps,
+    #     n_spins,
+    #     std=simulation.results.mag_std_over_reps,
+    # )
+    #
 
 
 if __name__ == "__main__":
